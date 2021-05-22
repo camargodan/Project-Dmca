c4e353f9b6036f2b4fd6eca404ca7fb14e4d3787
heber <heber69camargo@gmail.com>
2021-05-21 21:56:10 -0400

    after login, succefull redirect.

--- dmcaprivacy/dmca/urls.py
+++ dmcaprivacy/dmca/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Index
+from .views import Loginview, Administrator, Worker, User
 
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    # path('login/', views.login, name='login'),
-    path('', Index.as_view(), name='index'),
+    path('', Loginview.as_view(), name='loginview'),
+    path('administrator/', Administrator.as_view(), name='administrator'),
+    path('worker/', Worker.as_view(), name='worker'),
+    path('user/', User.as_view(), name='user'),
 ]
--- dmcaprivacy/dmca/views.py
+++ dmcaprivacy/dmca/views.py
@@ -1,17 +1,36 @@
 from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
 
 
-# class SuperUserRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
-#
-#     def test_func(self):
-#         if self.request.user.is_superuser:
-#             return self.request.user.is_client
-#         else:
-#             return self.request.user.is_superuser
-
-
-class Index(LoginRequiredMixin, TemplateView):
+class Loginview(LoginRequiredMixin, TemplateView):
     login_url = '/accounts/login/'
     redirect_field_name = 'home'
-    template_name = 'dmca/dashboard_client.html'
+    template_name = 'dmca/index.html'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_superuser:
+            return HttpResponseRedirect('administrator')
+        elif request.user.is_worker:
+            return HttpResponseRedirect('worker')
+        else:
+            return HttpResponseRedirect('user')
+        return super(TemplateView, self).get(request, *args, **kwargs)
+
+
+class Administrator(LoginRequiredMixin, TemplateView):
+    login_url = '/accounts/login/'
+    redirect_field_name = 'home'
+    template_name = 'dmca/administrator.html'
+
+
+class Worker(LoginRequiredMixin, TemplateView):
+    login_url = '/accounts/login/'
+    redirect_field_name = 'home'
+    template_name = 'dmca/worker.html'
+
+
+class User(LoginRequiredMixin, TemplateView):
+    login_url = '/accounts/login/'
+    redirect_field_name = 'home'
+    template_name = 'dmca/user.html'
