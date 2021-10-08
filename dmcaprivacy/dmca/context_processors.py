c852ba2a57643542329a3520d796c62fc925fcd2
heber <heber69camargo@gmail.com>
2021-10-08 19:23:14 -0400

    detail on context processor fixed.

---
+++ dmcaprivacy/dmca/context_processors.py
@@ -0,0 +1,28 @@
+from .models import GoogleReports, TubeReports, TubePages, Clients
+import jsonpickle
+from jsonpickle import json
+
+
+def custom_processor(request):
+    if not request.user.id:
+        data = 'none'
+        last_five = 'none'
+    else:
+        client = Clients.objects.get(user=request.user.id)
+        last_five = GoogleReports.objects.filter(clients_id_clie=client.id_clie).order_by('-date_gore')[:5]
+        data = {}
+        data = []
+        for e in TubeReports.objects.filter(clients_id_clie=client.id_clie):
+            encode_reports = jsonpickle.encode(e, unpicklable=False)
+            reports_json = json.loads(encode_reports)
+            dest = {}
+            dest.update(reports_json)
+            for b in TubePages.objects.filter(id_tube_pages=e.id_tube_pages_id):
+                encodepage = jsonpickle.encode(b, unpicklable=False)
+                pagejson = json.loads(encodepage)
+                dest.update(pagejson)
+            data.append(dest)
+
+    return {"last_five_google": last_five,
+            "last_tubes": data,
+            }
